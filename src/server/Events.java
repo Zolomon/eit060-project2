2eeaba23f1ec00d22036eca5edf921f57ed36c54
Tobias Andersson <dic11tan@student.lu.se>
2013-02-19 17:29:02 +0100

    Added tweaks to Log

--- src/server/Events.java
+++ src/server/Events.java
@@ -3,10 +3,16 @@ public class Events {
 	private int eventClassId;
 	private EntityWithAccessControl entity;
 	private Journal journal;
+	private boolean failEvent;
 	
-	public Events(int eventClassId, EntityWithAccessControl entity, Journal journal){
+	public Events(int eventClassId, EntityWithAccessControl entity, Journal journal, boolean failEvent){
 		this.eventClassId = eventClassId;
 		this.entity = entity;
 		this.journal = journal;
+		this.failEvent = failEvent;
+	}
+	
+	public boolean getIfFail(){
+		return failEvent;
 	}
 }
--- src/server/Log.java
+++ src/server/Log.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
 
-public class Log extends Events{
+public class Log {
 
 	private ArrayList<Events> events;
 	
 	public Log(){	
-		this.events = new ArrayList<Events>;
+		this.events = new ArrayList<Events>();
 	}
 	
 	public  void updateLog(Events event){
