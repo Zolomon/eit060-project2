014b73f6b8566966f12cc03b78bd2306d33e3520
Tobias Andersson <dic11tan@student.lu.se>
2013-02-23 16:57:30 +0100

    Is trying to fix client. I have not compiled, but I am not sure if it works before keystores

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,18 +1,32 @@
 package client;
 
 import java.security.*;
+import java.io.*;
+import java.net.*;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class Client {
 
 	public static void main(String[] args) {
 		while (true) {
+			Socket client = null;
+			PrintWriter out = null;
+			try {
+
+				client = new Socket("Address to server", 6789);
+				out = new PrintWriter(client.getOutputStream(), true);
+				if (client.isConnected() != true) {
+					return;
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			System.out.println("Running Client ...");
 		}
 
 	}
 
-	private boolean initiateHandshake() {
-
-		return false;
-	}
 }
